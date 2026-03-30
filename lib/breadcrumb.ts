328c32bff3f066265543312b976817d0f2e9aefb
jmonzonc <jmonzoncarabe@protonmail.com>
2026-03-30 14:09:48 +0200

    Add files via upload

---
+++ lib/breadcrumb.ts
@@ -0,0 +1,14 @@
+type Crumb = { name: string; url: string };
+
+export function buildBreadcrumbJsonLd(crumbs: Crumb[]) {
+  return {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: crumbs.map((c, i) => ({
+      "@type": "ListItem",
+      position: i + 1,
+      name: c.name,
+      item: c.url,
+    })),
+  };
+}
